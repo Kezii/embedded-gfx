35d17d880128b1d67756db7298f0041db3edccac
Kezi <keziolio123@gmail.com>
2024-03-04 00:27:11 +0100

    move fb endianess setting to normal struct field

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -4,18 +4,23 @@ use embedded_graphics_core::{
     pixelcolor::{IntoStorage, Rgb565},
 };
 
-pub struct DmaReadyFramebuffer<const W: usize, const H: usize, const BIG_ENDIAN: bool> {
+pub struct DmaReadyFramebuffer<const W: usize, const H: usize> {
     pub framebuffer: *mut [[u16; W]; H], // tfw no generic_const_exprs
+    big_endian: bool,
 }
 
-impl<const W: usize, const H: usize, const BIG_ENDIAN: bool> DmaReadyFramebuffer<W, H, BIG_ENDIAN> {
-    pub fn new(raw_framebuffer: *mut ::core::ffi::c_void) -> DmaReadyFramebuffer<W, H, BIG_ENDIAN> {
+impl<const W: usize, const H: usize> DmaReadyFramebuffer<W, H> {
+    pub fn new(
+        raw_framebuffer: *mut ::core::ffi::c_void,
+        big_endian: bool,
+    ) -> DmaReadyFramebuffer<W, H> {
         if raw_framebuffer.is_null() {
             panic!("Failed to allocate framebuffer");
         }
 
         DmaReadyFramebuffer {
             framebuffer: raw_framebuffer as *mut [[u16; W]; H],
+            big_endian,
         }
     }
 
@@ -24,14 +29,10 @@ impl<const W: usize, const H: usize, const BIG_ENDIAN: bool> DmaReadyFramebuffer
             unsafe {
                 let framebuffer = &mut *self.framebuffer;
 
-                match BIG_ENDIAN {
-                    false => {
-                        framebuffer[point.y as usize][point.x as usize] = color.into_storage();
-                    }
-                    true => {
-                        framebuffer[point.y as usize][point.x as usize] =
-                            color.into_storage().to_be();
-                    }
+                if self.big_endian {
+                    framebuffer[point.y as usize][point.x as usize] = color.into_storage().to_be();
+                } else {
+                    framebuffer[point.y as usize][point.x as usize] = color.into_storage();
                 }
             }
         }
@@ -50,9 +51,7 @@ impl<const W: usize, const H: usize, const BIG_ENDIAN: bool> DmaReadyFramebuffer
     }
 }
 
-impl<const W: usize, const H: usize, const BIG_ENDIAN: bool> DrawTarget
-    for DmaReadyFramebuffer<W, H, BIG_ENDIAN>
-{
+impl<const W: usize, const H: usize> DrawTarget for DmaReadyFramebuffer<W, H> {
     type Color = Rgb565;
     type Error = core::convert::Infallible;
 
@@ -69,15 +68,17 @@ impl<const W: usize, const H: usize, const BIG_ENDIAN: bool> DrawTarget
     }
 
     fn clear(&mut self, color: Self::Color) -> Result<(), Self::Error> {
-        self.as_mut_slice().fill(color.into_storage());
+        if self.big_endian {
+            self.as_mut_slice().fill(color.into_storage().to_be());
+        } else {
+            self.as_mut_slice().fill(color.into_storage());
+        }
 
         Ok(())
     }
 }
 
-impl<const W: usize, const H: usize, const BIG_ENDIAN: bool> OriginDimensions
-    for DmaReadyFramebuffer<W, H, BIG_ENDIAN>
-{
+impl<const W: usize, const H: usize> OriginDimensions for DmaReadyFramebuffer<W, H> {
     fn size(&self) -> embedded_graphics_core::geometry::Size {
         embedded_graphics_core::geometry::Size::new(W as u32, H as u32)
     }
